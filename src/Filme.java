dca12b223e66a127dce49edc5751161608bc7e72
anna <naksilva23@gmail.com>
2026-03-23 20:23:10 -0300

    adicionando a classe filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,10 @@
+//definição de uma classe, tudo o que uma classe tem e tudo o que faz.
+
 public class Filme {
     String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
 
 }
