a51018ee4090ec909e3136608cedc72a2ab5f985
anna karolina <jkorumaito@gmail.com>
2026-03-23 23:26:11 -0300

    criando um MainAtividade para separar atividade do projeto Principal

---
+++ src/MainAtividade.java
@@ -0,0 +1,45 @@
+public class MainAtividade {
+    public static void main(String[] args) {
+        //Filme: guarda onde o objeto está. new Filme: cria o objeto.
+        Filme meuFilme = new Filme(); //tipo: referencia
+
+        //"." é para acessar algo de alguem
+        //instanciação (criar um objeto com suas proprias caracteristicas)
+        meuFilme.nome = "Terrifier";
+        meuFilme.anoDeLancamento = 2016;
+        meuFilme.duracaoEmMinutos = 84;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.mediaDasAvaliacoes());
+
+        //classe pessoa
+        Pessoa pessoa = new Pessoa();
+        pessoa.saudacao();
+
+        //classe calculadora
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.dobrarNumero(5);
+        System.out.println(resultado);
+
+        //classe musica
+        Musica minhaMusica = new Musica();
+
+        minhaMusica.titulo = "Nome da Música";
+        minhaMusica.artista = "Nome do Artista";
+        minhaMusica.anoLancamento = 2023;
+
+        minhaMusica.exibeFichaTecnica();
+
+        minhaMusica.avalia(4.5);
+        minhaMusica.avalia(3.8);
+        minhaMusica.avalia(5.0);
+
+        double mediaAvaliacoes = minhaMusica.pegaMedia();
+        System.out.println("Média das avaliações: " + mediaAvaliacoes);
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -17,29 +17,5 @@ public class Principal {
         System.out.println(meuFilme.totalDeAvaliacoes);
         System.out.println(meuFilme.mediaDasAvaliacoes());
 
-        //classe pessoa
-        Pessoa pessoa = new Pessoa();
-        pessoa.saudacao();
-
-        //classe calculadora
-        Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.dobrarNumero(5);
-        System.out.println(resultado);
-
-        //classe musica
-        Musica minhaMusica = new Musica();
-
-        minhaMusica.titulo = "Nome da Música";
-        minhaMusica.artista = "Nome do Artista";
-        minhaMusica.anoLancamento = 2023;
-
-        minhaMusica.exibeFichaTecnica();
-
-        minhaMusica.avalia(4.5);
-        minhaMusica.avalia(3.8);
-        minhaMusica.avalia(5.0);
-
-        double mediaAvaliacoes = minhaMusica.pegaMedia();
-        System.out.println("Média das avaliações: " + mediaAvaliacoes);
     }
 }
