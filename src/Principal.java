13aa49247f5689327c064ed5e15b761dd65818c0
anna karolina <jkorumaito@gmail.com>
2026-03-23 21:29:37 -0300

    fazendo instancia e comentando

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,14 @@
 public class Principal {
     public static void main(String[] args) {
+        //Filme: guarda onde o objeto está. new Filme: cria o objeto.
+        Filme meuFilme = new Filme(); //tipo: referencia
 
+        //"." é para acessar algo de alguem
+        meuFilme.nome = "Terrifier";
+        meuFilme.anoDeLancamento = 2016;
+        meuFilme.duracaoEmMinutos = 84;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
     }
 }
