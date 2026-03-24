a7ed9dfd2f242f423447a36e75a1e07cbbc05f08
anna karolina <jkorumaito@gmail.com>
2026-03-23 21:46:45 -0300

    adicionando calculo de media de avaliacoes

--- src/Filme.java
+++ src/Filme.java
@@ -22,4 +22,8 @@ public class Filme {
 
     }
 
+    double mediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -15,5 +15,6 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println(meuFilme.somaDasAvaliacoes);
         System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.mediaDasAvaliacoes());
     }
 }
