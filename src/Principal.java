b0049be599f2ba62ce1646cac9537469c1eb9c67
anna karolina <jkorumaito@gmail.com>
2026-03-23 23:11:25 -0300

    adicionando classes: musica, carro, aluno

--- src/Principal.java
+++ src/Principal.java
@@ -25,5 +25,45 @@ public class Principal {
         Calculadora calculadora = new Calculadora();
         int resultado = calculadora.dobrarNumero(5);
         System.out.println(resultado);
+
+        //classe musica
+        Musica minhaMusica = new Musica();
+
+        minhaMusica.titulo = "Nome da Música";
+        minhaMusica.artista = "Nome do Artista";
+        minhaMusica.anoLancamento = 2023;
+
+        minhaMusica.exibeFichaTecnica();
+
+        minhaMusica.avalia(4.5);
+        minhaMusica.avalia(3.8);
+        minhaMusica.avalia(5.0);
+
+        double mediaAvaliacoes = minhaMusica.pegaMedia();
+        System.out.println("Média das avaliações: " + mediaAvaliacoes);
+
+        //classe carro
+        public class Carro {
+            String modelo;
+            int ano;
+            String cor;
+
+            void exibeFichaTecnica() {
+                System.out.println("Modelo: " + modelo);
+                System.out.println("Ano: " + ano);
+                System.out.println("Cor: " + cor);
+            }
+
+            int calculaIdade() {
+                return 2023 - ano;
+            }
+        }
+
+        //classe aluno
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Akemi";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
     }
 }
